af49bd86587592277c621981364328d34f87e58c
GustavBrandsen <93977130+GustavBrandsen@users.noreply.github.com>
2025-02-05 13:40:53 +0100

    module 1

--- module1/SIR.cpp
+++ module1/SIR.cpp
@@ -1,22 +1,50 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 
 // Function to take a step in the SIR model
 // state: vector of S, I, R
 // beta: infection rate
 // gamma: recovery rate
 // dt: time step
-std::vector<float> take_step(std::vector<float> state, float beta, float gamma, float dt){
+std::vector<float> take_step(std::vector<float> state, float beta, float gamma, float dt)
+{
     std::vector<float> new_state;
-    //todo: implement the SIR model
+    // todo: implement the SIR model
+    float S = state[0];
+    float I = state[1];
+    float R = state[2];
+    float N = S + I + R;
+    float Snew = -beta * I * (S / N) * dt + S;
+    float Inew = (beta * I * (S / N) - gamma * I) * dt + I;
+    float Rnew = gamma * I * dt + R;
+    new_state = {Snew,
+                 Inew,
+                 Rnew};
     return new_state;
+}
+
+void run_SIR(std::string name, int days, std::vector<float> state, float beta, float gamma, float dt, bool skip = false)
+{
+    std::ofstream file("csv_files/" + name + ".csv");
+    float j = days / dt;
+    for (int i = 0; i < j; i++)
+    {
+        state = take_step(state, beta, gamma, dt);
+        if (skip && i % 10 != 0)
+        {
+            continue;
+        }
+        file << i*dt << "," << state[0] << ", " << state[1] << ", " << state[2] << "\n";
+    }
 }
 
 //======================================================================================================
 //======================== Main function ===============================================================
 //======================================================================================================
-int main(int argc, char* argv[]){    
+int main(int argc, char *argv[])
+{
     // define the parameters
 
     // TODO: Define the parameters of the SIR model
@@ -24,4 +52,11 @@ int main(int argc, char* argv[]){
     // TODO: implement the SIR model
 
     // TODO: Save the results to a file
+
+    std::vector<float> state = {9999., 1., 0.};
+
+    run_SIR("SIR_task2_1", 200, state, 0.2, 0.1, 0.1);
+    run_SIR("SIR_task2_2", 200, state, 0.2, 0.1, 0.00001, true);
+
+    return 0;
 }
---
+++ module1/plotting.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import pathlib
+
+path_to_file = os.path.abspath(__file__)
+path_to_csv_files = path_to_file.rsplit("/", 1)[0] + "/"
+
+def make_pic(filename, gamma, beta, dt):
+    df = pd.read_csv(path_to_csv_files + 'csv_files/' + filename + '.csv', header=None, delimiter=",", names=["t",'S', 'I', 'R'])
+    steps = df['t']
+    fig, ax = plt.subplots(2,2, figsize=(12,12))
+    ax[0,0].plot(steps, df['S'], label='S')
+    ax[0,0].set_title('S')
+    ax[0,0].set_xlabel('Days')
+    ax[0,0].set_ylabel('Uninfected people')
+    ax[0,1].plot(steps, df['I'], label='I')
+    ax[0,1].set_title('I')
+    ax[0,1].set_xlabel('Days')
+    ax[0,1].set_ylabel('Infected people')
+    ax[1,0].plot(steps, df['R'], label='R')
+    ax[1,0].set_title('R')
+    ax[1,0].set_xlabel('Days')
+    ax[1,0].set_ylabel('Recovered people')
+    ax[1,1].plot(steps, df['S'], label='S')
+    ax[1,1].plot(steps, df['I'], label='I')
+    ax[1,1].plot(steps, df['R'], label='R')
+    ax[1,1].set_title('All')
+    ax[1,1].set_xlabel('Days')
+    ax[1,1].set_ylabel('All people')
+    ax[1,1].legend()
+    fig.suptitle(r'SIR model with $\gamma$ = {gamma}, $\beta$ = {beta}, $\Delta$t = {dt}'.format(gamma=gamma, beta=beta, dt = dt))
+    os.chdir(path_to_csv_files)
+    plt.savefig('images/' + filename + '.png')
+
+make_pic('SIR_task2_1', 0.1, 0.2, 0.1)
+make_pic('SIR_task2_2', 0.1, 0.2, 0.00001)
